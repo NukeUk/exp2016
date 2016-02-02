333b99aba0a22f8cd00aa551eff3a2313ac6a25d
Nikeuk <nikeuk@gmail.com>
2016-02-02 19:51:13 +0300

    Added dayTimeService

--- services/daytimeService.js
+++ services/daytimeService.js
@@ -1,5 +1,17 @@
 function daytimeService(date) {
-	return "morning";
+	var curDayTime = 'day';
+	if(date.getHours() <= 12){
+		curDayTime = 'morning';
+	}
+	else if(date.getHours() <= 17){
+		curDayTime = 'day';
+	}
+	else if(date.getHours() <=24){
+		curDayTime = 'evening';
+	}
+	console.log(curDayTime);
+
+	return curDayTime;
 }
 
 module.exports = daytimeService;
